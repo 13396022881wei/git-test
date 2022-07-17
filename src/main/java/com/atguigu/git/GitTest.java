1f63101ffac65159a83cc50ef61ac686b7d436ec
Henry <Henry@qq.com>
2022-07-17 16:36:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * FileName:     GitTest
+ * CreateBy:     IntelliJ IDEA
+ * Author:       wei
+ * Date:         2022-07-17
+ * Description :
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
